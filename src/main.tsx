7ea26c2f3573db866f784f05bfe10822ebe0a8af
Wang Bin <2799400448@qq.com>
2024-04-25 20:48:33 +0800

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+// Copyright 2018-present the xiejiahe. All rights reserved. MIT license.
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/styles/global.scss'
+import AppRoute from './router'
+import 'antd/dist/reset.css'
+import { Provider } from 'react-redux'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import dayjs from 'dayjs'
+import zh from 'dayjs/locale/zh-cn'
+import store from '@/store'
+
+dayjs.locale(zh)
+
+const root = ReactDOM.createRoot(
+  document.getElementById("tomato-work-root") as HTMLElement
+)
+
+root.render(
+  // <React.StrictMode>
+  <Provider store={store}>
+    <ConfigProvider locale={zhCN}>
+      <AppRoute />
+    </ConfigProvider>
+  </Provider>
+  // </React.StrictMode>
+)
---
+++ src/views/memorandum/index.tsx
@@ -0,0 +1,111 @@
+/**
+ * 我的备忘
+ */
+import React, { useState, useEffect } from 'react'
+import './style.scss'
+import dayjs from 'dayjs'
+import NoData from '@/components/no-data/index'
+import { Card, Col, Row, Button, Popconfirm, Spin } from 'antd'
+import { Link, useNavigate } from 'react-router-dom'
+import { serviceGetMemorandum, serviceDeleteMemorandum } from '@/services'
+import { defaultTitle } from './constants'
+
+const MemorandumPage: React.FC = () => {
+  const navigate = useNavigate()
+  const [list, setList] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  function handleButton(
+    buttonType: 0 | 1 | 2,
+    item: any,
+    e?: React.MouseEvent
+  ) {
+    e?.stopPropagation()
+    e?.preventDefault()
+
+    if (buttonType === 0) {
+      setLoading(true)
+      serviceDeleteMemorandum(item.id)
+        .then(() => {
+          getData()
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+      return
+    }
+
+    if (buttonType === 2) {
+      navigate('/home/memorandum/create')
+      return
+    }
+
+    navigate(`/home/memorandum/update/${item.id}`)
+  }
+
+  function getData() {
+    serviceGetMemorandum()
+    .then(res => {
+      const data = res.map((item: any) => {
+        item.createdAt = dayjs(item.createdAt).format('YYYY/M/D HH:mm')
+        item.title = item.title || defaultTitle
+        return item
+      })
+      setList(data)
+    })
+    .finally(() => setLoading(false))
+  }
+
+  function goDetail(id: string) {
+    navigate(`/home/memorandum/detail/${id}`)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <Spin spinning={loading} wrapperClassName="memorandum-spin">
+      <div className="memorandum">
+        {(list.length > 0) ? (
+          <Row gutter={16} align="bottom">
+            {list.map((item: any) => (
+              <Col span={8} key={item.id}>
+                <Card onClick={() => goDetail(item.id)} title={item.title} hoverable>
+                  {item.createdAt}
+                  <div
+                    className="content"
+                    dangerouslySetInnerHTML={{ __html: item.html }}
+                  >
+                  </div>
+                  <div className="button-group">
+                    <Popconfirm
+                      title="您确定要删除吗？"
+                      onConfirm={e => {
+                        e?.stopPropagation()
+                        handleButton(0, item)
+                      }}
+                      onCancel={e => { e?.stopPropagation()}}
+                      placement="bottomRight"
+                      okType="danger"
+                    >
+                      <Button size="small" onClick={(e) => e?.stopPropagation()}>删除</Button>
+                    </Popconfirm>
+                    <Button size="small" onClick={handleButton.bind(null, 1, item)}>编辑</Button>
+                  </div>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <NoData
+            message="还没有备忘录，是否马上创建？"
+            onClick={handleButton.bind(null, 2, null)}
+          />
+        )}
+      </div>
+    </Spin>
+  )
+}
+
+export default MemorandumPage
